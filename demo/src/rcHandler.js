e87dc7b8123d025b1c6586a7ccb6ae81e2820f04
Nguyễn Anh Tuấn <50624746+Clarence161095@users.noreply.github.com>
2024-09-29 21:01:40 +0900

    init source

---
+++ demo/src/rcHandler.js
@@ -0,0 +1,28 @@
+const AWS = require("aws-sdk");
+const sqs = new AWS.SQS();
+
+exports.handler = async (event) => {
+  const record = event.Records[0];
+  const bucketName = record.s3.bucket.name;
+  const fileName = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+
+  const message = {
+    bucket: bucketName,
+    file: fileName,
+    type: "rc",
+  };
+
+  const params = {
+    MessageBody: JSON.stringify(message),
+    QueueUrl: process.env.SQS_QUEUE_URL,
+  };
+
+  try {
+    await sqs.sendMessage(params).promise();
+    console.log("Message sent to SQS");
+    return { statusCode: 200, body: "Message sent to SQS" };
+  } catch (error) {
+    console.error("Error sending message to SQS:", error);
+    return { statusCode: 500, body: "Error sending message to SQS" };
+  }
+};
